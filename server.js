67c3cc02291e3c61d77af76405566ae9970cbcd5
Ankur Dahal <dahalankur123@gmail.com>
2021-12-28 16:30:14 +0545

    Finish up setting routes and database operations

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const shortURL = require('./models/shortURL')
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
-app.listen(PORT)
+mongoose.connect(process.env.MONGO_URI)
+
+// use ejs as the default view engine
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+    const shortURLs = await shortURL.find()
+    res.render('index', { shortURLs : shortURLs })
+})
+
+app.post('/shortenURL', async (req, res) => {
+    await shortURL.create({ fullURL: req.body.fullURL })
+    res.redirect('/')      
+})
+
+app.get('/:shortURL', async (req, res) => {
+    const url = await shortURL.findOne({ shortURL: req.params.shortURL })
+    if (url == null) return res.sendStatus(404)
+    url.clicks += 1
+    await url.save()
+
+    res.redirect(url.fullURL)
+})  
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`))
